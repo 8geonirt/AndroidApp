aba4685d2716ce7f4bdb13992d53b12a0a2ffa96
José Trinidad Espinoza González <trinoeg8@gmail.com>
2015-01-25 00:16:52 -0600

    Unused code deleted

--- app/src/main/java/com/trinoeg8/trino/oneloginexampleapp/Clases/Company.java
+++ app/src/main/java/com/trinoeg8/trino/oneloginexampleapp/Clases/Company.java
@@ -4,16 +4,8 @@ package com.trinoeg8.trino.oneloginexampleapp.Clases;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.drawable.Drawable;
-import android.os.AsyncTask;
-import android.util.Log;
 
-import com.trinoeg8.trino.oneloginexampleapp.CompaniesAdapter;
 import com.trinoeg8.trino.oneloginexampleapp.R;
-
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
 
 /**
  * Created by Trino on 17/01/2015.
@@ -25,7 +17,6 @@ public class Company {
     private String url;
     private String category;
     private Bitmap image;
-    private CompaniesAdapter adapter;
     private Context context;
     public Company(){
 
